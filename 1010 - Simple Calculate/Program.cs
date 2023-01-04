b8e27836fc882b2c894a263734dd115e53724c88
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-03 22:23:47 -0300

    Add files via upload

---
+++ 1010 - Simple Calculate/Program.cs
@@ -0,0 +1,13 @@
+﻿double code, unit, price, total;
+string [] vet = Console.ReadLine().Split(' ');
+code = double.Parse(vet[0]);
+unit = double.Parse(vet[1]);
+price = double.Parse(vet[2]);
+double code2, unit2, price2;
+string [] vet2 = Console.ReadLine().Split(' ');
+code2 = double.Parse(vet2[0]);
+unit2 = double.Parse(vet2[1]);
+price2 = double.Parse(vet2[2]);
+total = (unit * price) + (unit2 * price2);
+System.Console.WriteLine("VALOR A PAGAR: R$ "+ total.ToString("F2"));
+
