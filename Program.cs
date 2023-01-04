13e2d1059d2134fd9ad867a5cc574d7c6d1ebf2d
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-03 22:11:31 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
-﻿int number, hours;
-number = Convert.ToInt32(Console.ReadLine());
-hours = Convert.ToInt32(Console.ReadLine());
-float payment, salary;
-payment = Convert.ToSingle(Console.ReadLine());
-salary = hours * payment;
-System.Console.WriteLine($"NUMBER = " + number);
-System.Console.WriteLine($"SALARY = U$ "+ salary.ToString("F2"));
+﻿string name;
+double salary, value, finalSalary; 
+name = Console.ReadLine();
+salary = double.Parse(Console.ReadLine());
+value = double.Parse(Console.ReadLine());
+finalSalary = (value * 15)/100+ salary;
+System.Console.WriteLine("TOTAL = R$ "+ finalSalary.ToString("F2"));
