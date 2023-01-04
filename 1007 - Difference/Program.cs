453f0cd720ce73ee1c812d7dc3e51a8531640864
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-03 22:09:43 -0300

    Add files via upload

---
+++ 1007 - Difference/Program.cs
@@ -0,0 +1,7 @@
+﻿int A, B, C, D;
+A = Convert.ToInt32(Console.ReadLine());
+B = Convert.ToInt32(Console.ReadLine());
+C = Convert.ToInt32(Console.ReadLine());
+D = Convert.ToInt32(Console.ReadLine());
+int difference = A * B - C * D;
+System.Console.WriteLine("DIFERENCA = "+ difference);
