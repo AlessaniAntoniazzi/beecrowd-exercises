01d06def37ff6fa9f2a5a4107ee95d2e4014fd4e
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-03 22:35:29 -0300

    Add files via upload

---
+++ 1020 - Age in Days/Program.cs
@@ -0,0 +1,9 @@
+﻿int dia, mes, ano, n, resto;
+n = int.Parse(Console.ReadLine());
+ano = n/365;
+resto = n%365;
+mes = resto/30;
+dia = resto%30;
+System.Console.WriteLine(ano+" ano(s)");
+System.Console.WriteLine(mes+" mes(es)");
+System.Console.WriteLine(dia+" dia(s)");
