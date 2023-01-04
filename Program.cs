413875911fb7455090f0b94159160e1d91d95b62
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-03 22:10:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿int number, hours;
+number = Convert.ToInt32(Console.ReadLine());
+hours = Convert.ToInt32(Console.ReadLine());
+float payment, salary;
+payment = Convert.ToSingle(Console.ReadLine());
+salary = hours * payment;
+System.Console.WriteLine($"NUMBER = " + number);
+System.Console.WriteLine($"SALARY = U$ "+ salary.ToString("F2"));
