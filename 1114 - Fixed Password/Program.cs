aa9353c0bc36b1760a44c63a3a3fb2e21aa003ee
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-03 22:37:40 -0300

    Add files via upload

---
+++ 1114 - Fixed Password/Program.cs
@@ -0,0 +1,14 @@
+﻿int password = 0;
+while (password != 2002)
+{
+    password = int.Parse(Console.ReadLine()); 
+    if (password != 2002)
+    {
+        System.Console.WriteLine("Senha Invalida");
+    }
+    else if (password == 2002)
+    {
+        Console.WriteLine("Acesso Permitido");
+    }
+}
+Console.ReadKey();
