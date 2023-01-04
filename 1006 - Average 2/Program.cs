914cc1157ad754e0e41b0bc3afe3598be0a236a8
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-04 00:14:19 -0300

    Add files via upload

---
+++ 1006 - Average 2/Program.cs
@@ -0,0 +1,6 @@
+﻿double A = double.Parse(Console.ReadLine());
+double B = double.Parse(Console.ReadLine());
+double C = double.Parse(Console.ReadLine());
+
+double average = ((A * 2.0) + (B * 3.0) + (C * 5.0))/ (2.0+3.0+5.0);
+System.Console.WriteLine("MEDIA = "+average.ToString("F1"));
