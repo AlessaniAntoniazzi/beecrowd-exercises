b6cb5226315077f66d35d7eccb8158656a9bbd5e
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-03 22:28:12 -0300

    Add files via upload

---
+++ 1014 - Consumption/Program.cs
@@ -0,0 +1,7 @@
+﻿int x;
+double y, average;
+x = int.Parse(Console.ReadLine());
+y = double.Parse(Console.ReadLine());
+average = x/y;
+System.Console.WriteLine(average.ToString("F3") + " km/l");
+      
