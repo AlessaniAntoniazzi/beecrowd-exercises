cb223cd935f141cc836f3bac0e43db145f102715
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-04 19:54:55 -0300

    Add files via upload

---
+++ 1035 - Selection Test 1/Program.cs
@@ -0,0 +1,14 @@
+﻿int A, B, C, D;
+string [] var = Console.ReadLine().Split(' ');
+A = int.Parse(var[0]);
+B = int.Parse(var[1]);
+C = int.Parse(var[2]);
+D = int.Parse(var[3]);
+if (B>C && D>A && C+D > A+B && C > 0 && D>0 && A%2==0)
+{
+    System.Console.WriteLine("Valores aceitos");
+}
+else
+{
+    System.Console.WriteLine("Valores nao aceitos");
+}
