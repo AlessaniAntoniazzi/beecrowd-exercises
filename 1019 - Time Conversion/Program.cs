e6d70417a8ba99fe53158713e346408336df0564
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-03 22:34:18 -0300

    Add files via upload

---
+++ 1019 - Time Conversion/Program.cs
@@ -0,0 +1,7 @@
+﻿int N, horas, minutos, segundos, resto;
+N = int.Parse(Console.ReadLine());
+horas = N / 3600;
+resto = N % 3600;
+minutos = resto/ 60;
+segundos = resto %  60;
+System.Console.WriteLine(horas+":"+minutos+":"+segundos);
