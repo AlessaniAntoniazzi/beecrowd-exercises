475249dfee6385de7465acf07bdaa2760714869c
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-03 22:36:45 -0300

    Add files via upload

---
+++ 1113 - Ascending and Descending/Program.cs
@@ -0,0 +1,19 @@
+﻿int x = 1;
+int y = 0;
+while (x != y)
+{
+    string input = Console.ReadLine();
+    string[] parts = input.Split(' ');
+    x = int.Parse(parts[0]);
+    y = int.Parse(parts[1]);
+  
+
+    if (x < y)
+    {
+        Console.WriteLine("Crescente");
+    }
+    else if (y < x)
+    {
+        Console.WriteLine("Decrescente");
+    }
+}   
