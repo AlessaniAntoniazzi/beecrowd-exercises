be2c953913f83754d0a3882f18da8528070e850a
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-03 22:12:17 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-﻿string name;
-double salary, value, finalSalary; 
-name = Console.ReadLine();
-salary = double.Parse(Console.ReadLine());
-value = double.Parse(Console.ReadLine());
-finalSalary = (value * 15)/100+ salary;
-System.Console.WriteLine("TOTAL = R$ "+ finalSalary.ToString("F2"));
+﻿double code, unit, price, total;
+string [] vet = Console.ReadLine().Split(' ');
+code = double.Parse(vet[0]);
+unit = double.Parse(vet[1]);
+price = double.Parse(vet[2]);
+double code2, unit2, price2;
+string [] vet2 = Console.ReadLine().Split(' ');
+code2 = double.Parse(vet2[0]);
+unit2 = double.Parse(vet2[1]);
+price2 = double.Parse(vet2[2]);
+total = (unit * price) + (unit2 * price2);
+System.Console.WriteLine("VALOR A PAGAR: R$ "+ total.ToString("F2"));
+
