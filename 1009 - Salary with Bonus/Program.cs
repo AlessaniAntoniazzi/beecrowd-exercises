e7d5ee65ecb1c985cea381ef0bdf26386ed3dcf4
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-03 22:22:19 -0300

    Add files via upload

---
+++ 1009 - Salary with Bonus/Program.cs
@@ -0,0 +1,7 @@
+﻿string name;
+double salary, value, finalSalary; 
+name = Console.ReadLine();
+salary = double.Parse(Console.ReadLine());
+value = double.Parse(Console.ReadLine());
+finalSalary = (value * 15)/100+ salary;
+System.Console.WriteLine("TOTAL = R$ "+ finalSalary.ToString("F2"));
