13901157b5c5d2d8cccdbbb1aa833d8fa68c3df6
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-03 22:32:57 -0300

    Add files via upload

---
+++ 1017 - Fuel Spent/Program.cs
@@ -0,0 +1,5 @@
+﻿double time, speed, liters;
+time = double.Parse(Console.ReadLine());
+speed = int.Parse(Console.ReadLine());
+liters = (speed/12) * time;
+System.Console.WriteLine(liters.ToString("F3"));
