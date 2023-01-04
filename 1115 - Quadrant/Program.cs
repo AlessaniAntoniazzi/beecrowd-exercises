0fa2bac3706f952f687fc4240be7f6da6e2b9aba
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-03 22:38:42 -0300

    Add files via upload

---
+++ 1115 - Quadrant/Program.cs
@@ -0,0 +1,24 @@
+﻿int x = 1;
+int y = 1;
+while (x != 0 && y != 0)
+{
+    string [] var = Console.ReadLine().Split(' ');
+    x = int.Parse(var[0]);
+    y = int.Parse(var[1]);
+    if (x > 0 && y > 0)
+    {
+        System.Console.WriteLine("primeiro");
+    }
+    else if (x > 0 && y < 0)
+    {
+        System.Console.WriteLine("quarto");
+    }
+    else if ( x < 0 && y < 0)
+    {
+        System.Console.WriteLine("terceiro");
+    }
+    else if ( x < 0 && y > 0)
+    {
+        System.Console.WriteLine("segundo");
+    }
+}
