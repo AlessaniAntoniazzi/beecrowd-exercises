002b85e4d81ce2d550c76ad4bfe69ad79daad6f2
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-03 22:26:36 -0300

    Add files via upload

---
+++ 1013 - The Greatest/Program.cs
@@ -0,0 +1,9 @@
+﻿int a, b, c, maiorAB, maior;
+string [] vet = Console.ReadLine().Split();
+a = int.Parse(vet [0]);
+b = int.Parse(vet [1]);
+c = int.Parse(vet [2]);
+maiorAB = (a + b + Math.Abs(a - b))/2;
+maior = (maiorAB + c + Math.Abs(maiorAB - c))/2;
+System.Console.WriteLine(maior + " eh o maior");
+
