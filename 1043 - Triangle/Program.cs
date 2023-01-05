d27e9e70a9cb6a793fb6a5a44933689825710259
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-05 17:08:27 -0300

    Add files via upload

---
+++ 1043 - Triangle/Program.cs
@@ -0,0 +1,15 @@
+﻿double A, B, C, perimeter, area;
+string [] var = Console.ReadLine().Split(' ');
+A = double.Parse(var[0]);
+B = double.Parse(var[1]);
+C = double.Parse(var[2]);
+if ((A + B > C) && (B + C > A) && (A + C > B))
+{
+    perimeter = A+B+C;
+    Console.WriteLine("Perimetro = "+perimeter.ToString("F1"));
+}
+else 
+{
+    area = (A + B) * C / 2;
+    Console.WriteLine("Area = "+area.ToString("F1"));
+}
