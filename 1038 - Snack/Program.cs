a90a1baf54bd4db6603d7b10dc18972bbd2b97ad
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-05 15:36:18 -0300

    Add files via upload

---
+++ 1038 - Snack/Program.cs
@@ -0,0 +1,34 @@
+﻿double price, cachorroQuente, xSalada, xBacon, torrada, refri;
+cachorroQuente = 4.00;
+xSalada = 4.50;
+xBacon = 5.00;
+torrada = 2.00;
+refri = 1.50;
+string [] var = Console.ReadLine().Split(' ');
+int x = int.Parse(var[0]);
+int y = int.Parse(var[1]);
+if (x==1)
+{
+    price = y * cachorroQuente;
+    System.Console.WriteLine("Total: R$ "+price.ToString("F2"));
+}
+else if (x==2)
+{
+    price = y * xSalada;
+    System.Console.WriteLine("Total: R$ "+price.ToString("F2"));
+}
+else if (x==3)
+{
+    price = y * xBacon;
+    System.Console.WriteLine("Total: R$ "+price.ToString("F2"));
+}
+else if (x==4)
+{
+    price = y * torrada;
+    System.Console.WriteLine("Total: R$ "+price.ToString("F2"));
+}
+else
+{
+    price = y * refri;
+    System.Console.WriteLine("Total: R$ "+price.ToString("F2"));
+}
