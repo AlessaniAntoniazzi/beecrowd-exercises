40b093c7cff2303954253362a4446f03b6862636
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-05 17:09:33 -0300

    Add files via upload

---
+++ 1037 - Interval/Program.cs
@@ -0,0 +1,21 @@
+﻿double x = double.Parse(Console.ReadLine());
+if (x < 0.00 || x > 100)
+{
+    System.Console.WriteLine("Fora de intervalo");
+}
+else if ( x<=25)
+{
+    System.Console.WriteLine("Intervalo [0,25]");
+}
+else if (x <= 50)
+{
+    System.Console.WriteLine("Intervalo (25,50]");
+}
+else if (x <= 75)
+{
+    System.Console.WriteLine("Intervalo (50,75]");
+}
+else 
+{
+    System.Console.WriteLine("Intervalo (75,100]");
+}
