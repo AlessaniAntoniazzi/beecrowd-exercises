016ec9bab02cb5471a3cdbf50128d9dd3420f090
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-10 12:28:39 -0300

    Add files via upload

---
+++ 1046 - Game Time/Program.cs
@@ -0,0 +1,19 @@
+﻿int x, y, z;
+string [] var = Console.ReadLine().Split(' ');
+x = int.Parse(var[0]);
+y = int.Parse(var[1]);
+if (x > y)
+{
+    z = ((x - 24) * -1) + y; 
+   System.Console.WriteLine("O JOGO DUROU " + z + " HORA(S)");
+}
+else if ( x == y)
+{
+    z = 24;
+    System.Console.WriteLine("O JOGO DUROU " + z + " HORA(S)");
+}
+else
+{
+    z = y - x; 
+    System.Console.WriteLine("O JOGO DUROU " + z + " HORA(S)");
+}
