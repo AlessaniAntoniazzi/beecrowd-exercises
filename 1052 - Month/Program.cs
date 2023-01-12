83bd64b6852f993fa412532004de77a0662e2e38
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-12 20:15:07 -0300

    Add files via upload

---
+++ 1052 - Month/Program.cs
@@ -0,0 +1,49 @@
+﻿int x = int.Parse(Console.ReadLine());
+if (x == 1)
+{
+    System.Console.WriteLine("January");
+}
+else if ( x == 2)
+{
+    System.Console.WriteLine("February");
+}
+else if (x == 3)
+{
+    System.Console.WriteLine("March");
+}
+else if (x == 4)
+{
+    System.Console.WriteLine("April");
+}
+else if (x == 5)
+{
+    System.Console.WriteLine("May");
+}
+else if (x == 6)
+{
+    System.Console.WriteLine("June");
+}
+else if (x == 7)
+{
+    System.Console.WriteLine("July");
+}
+else if (x == 8)
+{
+    System.Console.WriteLine("August");
+}
+else if (x == 9)
+{
+    System.Console.WriteLine("September");
+}
+else if ( x == 10)
+{
+    System.Console.WriteLine("October");
+}
+else if ( x == 11)
+{
+    System.Console.WriteLine("November");
+}
+else if ( x == 12)
+{
+    System.Console.WriteLine("December");
+}
