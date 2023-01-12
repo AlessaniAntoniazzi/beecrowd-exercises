a23b3e19039acac897174b407747c5c471da6bdf
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-12 00:17:21 -0300

    Add files via upload

---
+++ 1050 - DDD/Program.cs
@@ -0,0 +1,37 @@
+﻿int x = int.Parse(Console.ReadLine());
+if (x == 61)
+{
+    System.Console.WriteLine("Brasilia");
+}
+else if( x == 71)
+{
+    System.Console.WriteLine("Salvador");
+}
+else if ( x == 11)
+{
+    System.Console.WriteLine("Sao Paulo");
+}
+else if ( x == 21)
+{
+System.Console.WriteLine("Rio de Janeiro");
+}
+else if ( x == 32)
+{
+    System.Console.WriteLine("Juiz de Fora");
+}
+else if ( x == 19)
+{
+    System.Console.WriteLine("Campinas");
+}
+else if ( x == 27)
+{
+    System.Console.WriteLine("Vitoria");  
+}
+else if ( x == 31)
+{
+    System.Console.WriteLine("Belo Horizonte");
+}
+else
+{
+    System.Console.WriteLine("DDD nao cadastrado");
+}
