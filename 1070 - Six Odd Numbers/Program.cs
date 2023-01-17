6e4746d27b1a9159b50bb42def93445e6c5be464
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-17 14:05:45 -0300

    Add files via upload

---
+++ 1070 - Six Odd Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿int x = int.Parse(Console.ReadLine());
+if (x  % 2 == 0)
+{
+    x=  x + 1;
+    System.Console.WriteLine(x);
+    x=  x + 2;
+    System.Console.WriteLine(x);
+    x=  x + 2;
+    System.Console.WriteLine(x);
+    x=  x + 2;
+    System.Console.WriteLine(x);
+    x=  x + 2;
+    System.Console.WriteLine(x);
+    x=  x + 2;
+    System.Console.WriteLine(x);
+
+}
+else
+{
+    System.Console.WriteLine(x);
+    x =  x + 2;
+    System.Console.WriteLine(x);
+    x=  x + 2;
+    System.Console.WriteLine(x);
+    x=  x + 2;
+    System.Console.WriteLine(x);
+    x=  x + 2;
+    System.Console.WriteLine(x);
+    x=  x + 2;
+    System.Console.WriteLine(x);
+}
