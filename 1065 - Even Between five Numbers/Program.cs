0ba8eabea42056aaed9a39c8ac69ce89e4b45528
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-17 13:34:58 -0300

    Add files via upload

---
+++ 1065 - Even Between five Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿int sum = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+
+    if(number %2 == 0)
+    {
+        sum += 1;
+    }
+}
+
+Console.WriteLine(sum + " valores pares");
+
