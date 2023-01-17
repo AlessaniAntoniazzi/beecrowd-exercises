1a54005cf39561ea751c246c0118b56710c1c256
AlessaniAntoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-17 20:33:58 -0300

    Add files via upload

---
+++ 1117 - Score Validation/Program.cs
@@ -0,0 +1,26 @@
+﻿double average;
+double x = double.Parse(Console.ReadLine());
+
+while (true)
+{
+    if (x > 10.0 || x < 0.0)
+    {
+        System.Console.WriteLine("nota invalida");
+        x = double.Parse(Console.ReadLine());
+          
+    }
+    else if (x < 10.0 && x > 0.0)
+    {
+        double y = double.Parse(Console.ReadLine());
+        if (y < 0.0 || y > 10.0)
+        {
+            System.Console.WriteLine("nota invalida");
+        }
+        else if (y > 0.0 || y < 10.0)
+        {
+            average = (x + y)/2;
+            System.Console.WriteLine("media = "+average);
+            break;
+        }
+    }
+}
