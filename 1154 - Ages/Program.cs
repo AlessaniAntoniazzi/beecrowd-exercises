a7651af1f24c1b1a7d0ee5bd37ab8a8cc2a6d34e
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-18 19:18:00 -0300

    Add files via upload

---
+++ 1154 - Ages/Program.cs
@@ -0,0 +1,23 @@
+﻿double countNumbers = 0;
+double sum = 0;
+double average;
+
+
+while (true)
+{
+    double x = double.Parse(Console.ReadLine());
+    if (x > 0)
+    {
+        countNumbers ++;
+        sum = sum + x;
+    }
+    else
+    {
+        average = sum/countNumbers;
+        System.Console.WriteLine(average.ToString("F2"));
+        break;
+    }
+    
+
+
+}
