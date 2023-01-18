78022d9a1a0936a14926d25a9a89b8ca5c3131fa
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-18 19:23:51 -0300

    Add files via upload

---
+++ 1134 - Type of Fuel/Program.cs
@@ -0,0 +1,31 @@
+﻿int countAlcool = 0;
+int countGasolina = 0;
+int countDiesel = 0;
+while (true)
+{
+    int x = int.Parse(Console.ReadLine());
+    if (x ==1)
+    {
+        countAlcool ++;
+    }
+    else if ( x == 2)
+    {
+        countGasolina ++;
+    }
+    else if ( x == 3)
+    {
+        countDiesel ++;
+    }
+    else if (x == 4)
+    {
+        System.Console.WriteLine("MUITO OBRIGADO");
+        System.Console.WriteLine("Alcool: "+countAlcool);
+        System.Console.WriteLine("Gasolina: "+countGasolina);
+        System.Console.WriteLine("Diesel: "+countDiesel);
+        break;
+    }
+    else
+    {
+        continue;
+    }
+}
