cb687b3f70f350bf7ac02dc438981b1c23718274
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-18 19:40:34 -0300

    Add files via upload

---
+++ 1159 - Sum of Consecutive Even Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿int x;
+int y;
+while (true)
+{
+    x = int.Parse(Console.ReadLine());
+    if (x == 0)
+    {
+        break;
+    }
+    else if (x%2 == 0)
+    {
+        y = x + (x+2)+(x+4)+(x+6)+(x+8);
+        System.Console.WriteLine(y);
+    }
+    else if (x%2 != 0)
+    {
+        x = x+1;
+        y = x + (x+2)+(x+4)+(x+6)+(x+8);
+        System.Console.WriteLine(y);
+    }
+    
+}
