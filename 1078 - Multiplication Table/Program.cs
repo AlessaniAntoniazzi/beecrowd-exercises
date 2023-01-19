f5bd0c1eeb3e99d4fea560e6e6350693800901a9
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-19 11:03:38 -0300

    Add files via upload

---
+++ 1078 - Multiplication Table/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int y = 1;
+int z;
+
+int x = int.Parse(Console.ReadLine());
+for (int i = 0; i < 10; i++) 
+{
+    z = y * x;
+    System.Console.WriteLine(y+" x "+ x +" = "+z);
+    y ++;  
+}
+
