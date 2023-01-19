e6c5c047d6675330686de131b6d2ca3a975a9b0b
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-19 17:30:29 -0300

    Add files via upload

---
+++ 1074 - Even or Odd/Program.cs
@@ -0,0 +1,31 @@
+﻿int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++ )
+{
+    int x = int.Parse(Console.ReadLine());
+    if (x == 0)
+    {
+        System.Console.WriteLine("NULL");
+    }
+    else if (x%2 == 0)
+    {
+        if(x > 0)
+        {
+            System.Console.WriteLine("EVEN POSITIVE");
+        }
+        else
+        {
+            System.Console.WriteLine("EVEN NEGATIVE");
+        }
+    }
+    else
+    {
+        if(x > 0)
+        {
+            System.Console.WriteLine("ODD POSITIVE");
+        }
+        else
+        {
+            System.Console.WriteLine("ODD NEGATIVE");
+        }
+    }
+}    
