bdd6acab25ad4c9e7573a5ad2559be4e39a12508
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-19 11:35:47 -0300

    Add files via upload

---
+++ 1071 - Sum of Consecutive Odd Numbers I/Program.cs
@@ -0,0 +1,25 @@
+﻿int min, max;
+int z = 0;
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+
+if (x < y)
+{
+    min = x;
+    max = y;
+}
+else
+{
+    min = y;
+    max = x;
+}
+
+
+for (int i = min+1; i < max; i++)
+{
+    if (i % 2 != 0) 
+    {
+        z = z+ i;
+    }
+}
+System.Console.WriteLine(z);
