1c0343164aa9191c7714f7890557879cc567bccd
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-18 21:31:54 -0300

    Add files via upload

---
+++ 1042 - Simple Sort/Program.cs
@@ -0,0 +1,30 @@
+﻿int a, b, c;
+string [] var = Console.ReadLine().Split(' ');
+a = int.Parse(var[0]);
+b = int.Parse(var[1]);
+c = int.Parse(var[2]);
+if (a > b)
+{
+int temp = a;
+a = b;
+b = temp;
+}
+if (b > c)
+{
+int temp = b;
+b = c;
+c = temp;
+}
+if (a > b)
+{
+int temp = a;
+a = b;
+b = temp;
+}
+Console.WriteLine(a);
+Console.WriteLine(b);                  
+Console.WriteLine(c);
+System.Console.WriteLine();
+System.Console.WriteLine(var[0]);   
+System.Console.WriteLine(var[1]);
+System.Console.WriteLine(var[2]);       
