2eecf7f68f777de6020f51a543c03d930c32c8c9
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-18 21:53:01 -0300

    Add files via upload

---
+++ 1060 - Positive Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿double numberCounter = 0;
+for (double i = 0; i < 6; i++)
+{
+    double number = double.Parse(Console.ReadLine());
+    if (number > 0)
+    {
+        numberCounter ++;
+    }
+}
+
+Console.WriteLine(numberCounter+ " valores positivos");
