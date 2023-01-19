1abf3caa551b1d8d0abbfe32ae67065c3ab6fde9
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-19 14:22:20 -0300

    Add files via upload

---
+++ 1072 - Interval 2/Program.cs
@@ -0,0 +1,20 @@
+﻿int n = int.Parse(Console.ReadLine());
+int countIn = 0;
+int countOut = 0;
+
+
+for (int i = 0; i < n; i ++)
+{
+    int x = int.Parse(Console.ReadLine());
+    if (x >= 10 && x <= 20)
+    {
+        countIn ++;
+    }
+    else
+    {
+        countOut ++;
+    }
+    
+}
+System.Console.WriteLine(countIn+" in");
+System.Console.WriteLine(countOut+" out");
