5a9b9a7ae66ad38e2cfa9346d848e71e8d681b13
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-20 16:41:23 -0300

    Add files via upload

---
+++ 1116 - Dividing X by Y/Program.cs
@@ -0,0 +1,19 @@
+﻿int N = int.Parse(Console.ReadLine());
+double z = 0;
+for (int i = 0; i < N; i++)
+{
+    string [] var = Console.ReadLine().Split(' ');
+    int x = int.Parse(var[0]);
+    int y = int.Parse(var[1]);
+    
+    if(y == 0)
+    {
+        System.Console.WriteLine("divisao impossivel");
+    }
+    else
+    {
+        z = (double)x/y;
+        System.Console.WriteLine(z.ToString("F1"));
+    }
+
+}
