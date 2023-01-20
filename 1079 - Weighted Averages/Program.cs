25dd9cbbb41e4a79872746a7676dd970287146d4
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-20 11:52:28 -0300

    Add files via upload

---
+++ 1079 - Weighted Averages/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split(' ');
+    double x = double.Parse(input[0]);
+    double y = double.Parse(input[1]);
+    double z = double.Parse(input[2]);
+    double total = (x * 2) + (y * 3) + (z * 5);
+    double weightTotal = 2 + 3 + 5;
+    double average = total / weightTotal;
+Console.WriteLine(average.ToString("F1"));
+}
