7ad1adea6f5e0f2c0659ba97e765c74e976339ce
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-20 11:48:40 -0300

    Add files via upload

---
+++ 1080 - Highest and Position/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+int[] numbers = new int[1000];
+for (int i = 0; i < 100; i++)
+{
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+int max = numbers.Max();
+int maxIndex = Array.IndexOf(numbers, max);
+Console.WriteLine(max);
+System.Console.WriteLine(maxIndex + 1);
+
