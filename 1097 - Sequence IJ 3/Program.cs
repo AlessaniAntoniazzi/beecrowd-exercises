2a7f2dc9d174ec56a3660716953f67e535213772
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-20 16:15:23 -0300

    Add files via upload

---
+++ 1097 - Sequence IJ 3/Program.cs
@@ -0,0 +1,12 @@
+﻿int I = 1;
+int J = 7;
+for (int i = 0; i < 5; i++)
+{
+    
+    System.Console.WriteLine("I="+I+" J="+J);
+    System.Console.WriteLine("I="+I+" J="+(J-1));
+    System.Console.WriteLine("I="+I+" J="+(J-2));
+    I = I +2;
+    J = J+2;
+
+}
