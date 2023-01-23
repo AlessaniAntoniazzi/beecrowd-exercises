866db7408bcbe430d00d713ff866ff76b9fa734b
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-23 17:24:18 -0300

    Add files via upload

---
+++ 1144 - Logical Sequence/Program.cs
@@ -0,0 +1,13 @@
+﻿int b = 1;
+int c = 1;
+int N = int.Parse(Console.ReadLine());
+for (int i = 1; i <= N;i++)
+{
+    b = i * i;
+    c = i* i* i;
+    System.Console.WriteLine(i+" "+b+" "+c);
+    System.Console.WriteLine(i+" "+(b+1)+" "+(c+1));
+}
+
+
+
