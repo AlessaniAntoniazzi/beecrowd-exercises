5fefeded330cbcadcc9255ebb4c2d89ec95ee246
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-23 16:31:05 -0300

    Add files via upload

---
+++ 1143 - Squared and Cubic/Program.cs
@@ -0,0 +1,13 @@
+﻿int N = int.Parse(Console.ReadLine());
+int a = 1;
+int b = 1;
+int c = 1;
+
+for (int i = 0; i < N; i++)
+{
+    System.Console.WriteLine(a+" "+b+" "+c);
+    a ++;
+    b = a*a;
+    c = a*a*a;
+    
+}
