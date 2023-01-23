4304aa12c07d2bb5a39b324e0d07b6df4e65d912
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-23 16:05:34 -0300

    Add files via upload

---
+++ 1142 - PUM/Program.cs
@@ -0,0 +1,11 @@
+﻿int x = 1;
+int y = 2;
+int z = 3;
+int N = int.Parse(Console.ReadLine());
+for (int i = 0; i < N; i ++)
+{
+    System.Console.WriteLine(x+" "+y+" "+z+" PUM");
+    x = x + 4;
+    y = y + 4;
+    z = z + 4;
+}
