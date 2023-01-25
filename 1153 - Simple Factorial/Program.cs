ac81a1e8d62ca0f2d9eff1555590aa953ffa7cdb
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-25 14:19:53 -0300

    Add files via upload

---
+++ 1153 - Simple Factorial/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = int.Parse(Console.ReadLine());
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= i;
+        
+    }
+    System.Console.WriteLine(result);
+}
