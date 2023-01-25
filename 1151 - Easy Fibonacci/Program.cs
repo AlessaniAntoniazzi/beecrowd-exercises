6abba21c8a9330e32da3300395fbb765a0aec5d5
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-25 09:03:47 -0300

    Add files via upload

---
+++ 1151 - Easy Fibonacci/Program.cs
@@ -0,0 +1,18 @@
+﻿string result = "";
+int n, first = 0, second = 1, next;
+int N = int.Parse(Console.ReadLine());
+for (int i = 0; i < N; i++)
+{
+    if (i <= 1)
+    {
+        next = i;
+    }
+    else
+    {
+        next = first + second;
+        first = second;
+        second = next;
+    }
+    result += next + " ";
+}
+Console.WriteLine(result.TrimEnd());
