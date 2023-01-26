7779ae92166bda40f7247f6feab9aec5cf569ba0
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-26 18:13:06 -0300

    Add files via upload

---
+++ 1132 - Multiples of 13/Program.cs
@@ -0,0 +1,23 @@
+﻿int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+int sum = 0;
+int min = 0;
+int max = 0;
+if (x > y)
+{
+    min = y;
+    max = x;
+}
+else
+{
+    min = x;
+    max = y;
+}
+for (int i = min; i <= max; i++)
+{
+    if (i%13!=0)
+    {
+        sum +=i;
+    }
+}
+System.Console.WriteLine(sum);
