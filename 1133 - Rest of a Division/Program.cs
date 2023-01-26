c83f7e5771e34aa52584f40e517ebf118d5656f9
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-26 18:14:49 -0300

    Add files via upload

---
+++ 1133 - Rest of a Division/Program.cs
@@ -0,0 +1,19 @@
+﻿int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+int min = 0;
+int max = 0;
+if (x > y)
+{
+    min = y;
+    max = x;
+}
+
+for (int i = min+ 1; i < max; i++)
+{
+    
+    if (i%5 ==2 || i%5==3)
+    {
+        System.Console.WriteLine(i);
+    }
+
+}
