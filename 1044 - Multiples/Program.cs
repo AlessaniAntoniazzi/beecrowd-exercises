b4b9b2e339d7f12a352ece53e25325d7d96f409d
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-28 23:52:17 -0300

    Add files via upload

---
+++ 1044 - Multiples/Program.cs
@@ -0,0 +1,12 @@
+﻿int A, B;
+string [] var = Console.ReadLine().Split(' ');
+A = int.Parse(var[0]);
+B = int.Parse(var[1]);
+if (B % A == 0|| A % B ==0 )
+{
+    System.Console.WriteLine("Sao Multiplos");
+}
+else
+{
+    System.Console.WriteLine("Nao sao Multiplos");
+}
