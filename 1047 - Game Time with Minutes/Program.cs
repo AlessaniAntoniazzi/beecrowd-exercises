df9f3a2f3d56d531d015165a7e9c7b9fa6dc7fd8
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-29 00:14:16 -0300

    Add files via upload

---
+++ 1047 - Game Time with Minutes/Program.cs
@@ -0,0 +1,25 @@
+﻿int firstHour, firstMinute, lasthour, lastMinute;
+string [] var = Console.ReadLine().Split(' ');
+firstHour = int.Parse(var[0]);
+firstMinute = int.Parse(var[1]);
+lasthour = int.Parse(var[2]);
+lastMinute = int.Parse(var[3]);
+
+int beginning = firstHour * 60 + firstMinute;
+int end = lasthour * 60 + lastMinute;
+
+int time;
+if (beginning < end) 
+{
+    time = end - beginning;
+}
+else
+{
+    time = (24 * 60 - beginning) + end;
+}
+
+int timeHours = time / 60;
+int timeMinutes = time % 60;
+
+Console.WriteLine("O JOGO DUROU " + timeHours + " HORA(S) E " + timeMinutes + " MINUTO(S)");
+        
