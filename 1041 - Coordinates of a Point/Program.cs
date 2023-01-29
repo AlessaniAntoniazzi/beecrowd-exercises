e37ca74b622e4c92328e5ff7218c3de1bb282f60
Alessani Antoniazzi <119366203+AlessaniAntoniazzi@users.noreply.github.com>
2023-01-28 23:53:49 -0300

    Add files via upload

---
+++ 1041 - Coordinates of a Point/Program.cs
@@ -0,0 +1,32 @@
+﻿double x, y;
+string [] var = Console.ReadLine().Split(' ');
+x = double.Parse(var[0]);
+y = double.Parse(var[1]);
+if (x > 0 && y > 0)
+{
+    System.Console.WriteLine("Q1");
+}
+else if (x < 0 && y > 0)
+{
+    System.Console.WriteLine("Q2");
+}
+else if (x < 0 && y < 0) 
+{
+   System.Console.WriteLine("Q3");
+}
+else if (x > 0 && y < 0) 
+{
+   System.Console.WriteLine("Q4");
+}
+else if (x == 0 && y == 0)
+{
+    System.Console.WriteLine("Origem");
+}
+else if (y ==0)
+{
+    System.Console.WriteLine("Eixo X");
+}
+else if (x == 0)
+{
+    System.Console.WriteLine("Eixo Y");
+}
